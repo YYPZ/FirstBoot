b9f6733befa9cc3daca6a5fe82c5e05dcc8b4655
zhenjin.ye <zhenjin.ye@KCDQGQEYUWIKAP3.innomactech.com>
2018-08-03 17:42:32 +0800

    # WARNING: head commit changed in the meantime
    update

--- src/main/java/com/ye/FirstBoot/controllor/UserControllor.java
+++ src/main/java/com/ye/FirstBoot/controllor/UserControllor.java
@@ -1,11 +1,18 @@
 package com.ye.FirstBoot.controllor;
 
+import java.io.IOException;
 import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ye.FirstBoot.domain.User;
@@ -23,14 +30,24 @@ public class UserControllor {
 	
 
 	@RequestMapping(path="getUserInfo",method= {RequestMethod.POST, RequestMethod.GET})
-	public ModelAndView getUserInfo(long id) {
+	public ModelAndView getUserInfo(long id,HttpServletRequest request,HttpServletResponse response) throws IOException {
 		 Optional<User> userOptional= userRepository.findById(id);
 		 ModelAndView mav = new ModelAndView("index");
 		 mav.addObject("user", userOptional.get());
-		
-		 mav.addObject("test","sdfghsg");
+	    System.out.println(request.getParameter("id"));
+	    request.setAttribute("id", "attr");
+	    System.out.println(request.getAttribute("id"));
+	
+	    ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+	    HttpServletRequest request2 = requestAttributes.getRequest();
+	    HttpServletResponse response2 = requestAttributes.getResponse();
+	    //从session里面获取对应的值
+	    String myValue = (String) requestAttributes.getAttribute("my_value",RequestAttributes.SCOPE_SESSION);
+	    System.out.println(myValue);
+	    requestAttributes.setAttribute("my_value", "FirstBoot", RequestAttributes.SCOPE_SESSION);
+	     myValue = (String) requestAttributes.getAttribute("my_value",RequestAttributes.SCOPE_SESSION);
+	     System.out.println(myValue);
 		return mav;
 	}
-
 
 }
