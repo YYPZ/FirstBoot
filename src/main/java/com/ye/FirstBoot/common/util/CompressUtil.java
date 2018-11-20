86b27147e3b36b83136f297006943e731b5ee16a
zhenjin.ye <zhenjin.ye@KCDQGQEYUWIKAP3.innomactech.com>
2018-11-20 20:53:46 +0800

    add buffer

--- src/main/java/com/ye/FirstBoot/common/util/CompressUtil.java
+++ src/main/java/com/ye/FirstBoot/common/util/CompressUtil.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -27,48 +26,46 @@ public class CompressUtil {
 
 	public static void main(String[] args) throws Exception {
 
-		compress("E:\\mydata", "E:\\mydata发送.zip");
+		compressToZip("E:\\mydata", "E:\\mydata发送.zip");
 		unZip("E:\\mydata发送.zip", "E:\\unzipT");
 		// decompressZip2Files("E:\\mydata发送.zip", "E:\\unzipT");
 
 	}
 
-	public static void compress(String sourceFilePath, String compressFile) throws Exception {
+	public static void compressToZip(String sourceFilePath, String compressFile) throws Exception {
 
 		ZipArchiveOutputStream zipOutput = null;
+
 		try {
 			File zipFile = new File(compressFile);
-			zipOutput = (ZipArchiveOutputStream) new ArchiveStreamFactory()
-					.createArchiveOutputStream(ArchiveStreamFactory.ZIP, new FileOutputStream(zipFile));
+			zipOutput = (ZipArchiveOutputStream) new ArchiveStreamFactory().createArchiveOutputStream(
+					ArchiveStreamFactory.ZIP, new BufferedOutputStream(new FileOutputStream(zipFile), BUFFER_SIZE));
 			zipOutput.setEncoding("UTF-8");
 			zipOutput.setUseZip64(Zip64Mode.AsNeeded);
 			Map<String, File> fileMap = findFile(sourceFilePath);
-			InputStream inputStream = null;
+			BufferedInputStream bufferedInputStream = null;
 			File file;
 			for (Map.Entry<String, File> mapEntry : fileMap.entrySet()) {
 				try {
 					file = mapEntry.getValue();
 					ZipArchiveEntry zipArchiveEntry = new ZipArchiveEntry(file, mapEntry.getKey());
+					zipArchiveEntry.setSize(file.length());
 					zipOutput.putArchiveEntry(zipArchiveEntry);
 					if (!file.isDirectory()) {
-						inputStream = new FileInputStream(file);
-						IOUtils.copy(inputStream, zipOutput);
+						bufferedInputStream = new BufferedInputStream(new FileInputStream(file), BUFFER_SIZE);
+						IOUtils.copy(bufferedInputStream, zipOutput);
 					}
 
 					zipOutput.closeArchiveEntry();
 
 				} finally {
-					if (inputStream != null) {
-						inputStream.close();
-					}
+					IOUtils.closeQuietly(bufferedInputStream);
 				}
 			}
 			zipOutput.finish();
 			zipOutput.close();
 		} catch (Exception e) {
-			if (zipOutput != null) {
-				zipOutput.close();
-			}
+			IOUtils.closeQuietly(zipOutput);
 			throw e;
 		}
 	}
@@ -101,67 +98,6 @@ public class CompressUtil {
 		}
 	}
 
-	/**
-	 * 将zip压缩包解压成文件到指定文件夹
-	 *
-	 * @param zipFilePath
-	 *            要解压的文件
-	 * @param targetDirPath
-	 *            解压的目的路径
-	 * @return 是否成功
-	 */
-	public static boolean decompressZip2Files(String zipFilePath, String targetDirPath) {
-
-		InputStream inputStream = null;
-		OutputStream outputStream = null;
-		// zip文件输入流
-		ZipArchiveInputStream zipArchiveInputStream = null;
-		ArchiveEntry archiveEntry = null;
-		try {
-			File zipFile = new File(zipFilePath);
-			inputStream = new FileInputStream(zipFile);
-			zipArchiveInputStream = new ZipArchiveInputStream(inputStream, "UTF-8");
-
-			while (null != (archiveEntry = zipArchiveInputStream.getNextEntry())) {
-				// 获取文件名
-				String archiveEntryFileName = archiveEntry.getName();
-				// 构造解压后文件的存放路径
-				String archiveEntryPath = targetDirPath + archiveEntryFileName;
-				// 把解压出来的文件写到指定路径
-				File entryFile = new File(archiveEntryPath);
-				if (!entryFile.exists()) {
-					boolean mkdirs = entryFile.getParentFile().mkdirs();
-
-				}
-				byte[] buffer = new byte[1024 * 5];
-				outputStream = new FileOutputStream(entryFile);
-				int len = -1;
-				while ((len = zipArchiveInputStream.read(buffer)) != -1) {
-					outputStream.write(buffer, 0, len);
-				}
-				outputStream.flush();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
-		} finally {
-			try {
-				if (null != outputStream) {
-					outputStream.close();
-				}
-				if (null != zipArchiveInputStream) {
-					zipArchiveInputStream.close();
-				}
-				if (null != inputStream) {
-					inputStream.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return true;
-	}
-
 	public static List unZip(String zipfile, String destDir) throws Exception {
 		File zipFile = new File(zipfile);
 		return unZip(zipFile, destDir);
@@ -175,31 +111,30 @@ public class CompressUtil {
 		ZipArchiveInputStream is = null;
 		List fileNames = new ArrayList();
 		try {
-			is = new ZipArchiveInputStream(new BufferedInputStream(new FileInputStream(zipfile), BUFFER_SIZE), "UTF-8");
+			is = new ZipArchiveInputStream(new BufferedInputStream(new FileInputStream(zipfile), BUFFER_SIZE));
 
 			ArchiveEntry entry = null;
 			File file;
 			String fileName;
 			while ((entry = is.getNextEntry()) != null) {
+				fileName = entry.getName();
 				fileNames.add(entry.getName());
+				file = new File(destDir, entry.getName());
 				if (entry.isDirectory()) {
-					File directory = new File(destDir, entry.getName());
-					directory.mkdirs();
-				} else {
-					OutputStream os = null;
-					fileName = entry.getName();
-					if (fileName.lastIndexOf("/") >= 0) {
-						fileName = fileName.substring(0, fileName.lastIndexOf("/"));
-						File directory = new File(destDir + "/" + fileName);
-						if (!directory.exists()) {
-							directory.mkdirs();
-						}
+					if (!file.isDirectory() && !file.mkdirs()) {
+						throw new IOException("failed to create directory " + file);
 					}
-					file = new File(destDir, entry.getName());
+				} else {
 
+					File parent = file.getParentFile();
+					if (!parent.isDirectory() && !parent.mkdirs()) {
+						throw new IOException("failed to create directory " + parent);
+					}
+					OutputStream os = null;
 					try {
 						os = new BufferedOutputStream(new FileOutputStream(file), BUFFER_SIZE);
 						IOUtils.copy(is, os);
+
 					} finally {
 						IOUtils.closeQuietly(os);
 					}
